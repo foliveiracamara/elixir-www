b338a64787884bf772c046292b1039ef6ad5c2ae
foliveiracamara <felipe.camara@bandtec.com.br>
2022-09-15 19:53:00 -0300

    feat: adding new button component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import styles from "./style.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <p>Hello world!</p>
+      
     </div>
   );
 }
