9d868f82eafe26788df6c6caaba19a06c054ccc3
foliveiracamara <felipe.camara@bandtec.com.br>
2022-09-19 21:03:20 -0300

    feat: adding blue cards and list component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,13 @@
-import styles from "./style.module.scss";
+import CardsList from '../../components/CardsList';
+
+import styles from './style.module.scss';
 
 export default function Home() {
+  
+
   return (
     <div className={styles.container}>
-      
+      <CardsList />
     </div>
   );
 }
