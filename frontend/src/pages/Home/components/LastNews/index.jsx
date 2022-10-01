daab51d7af41c28f4092d405d5a473b32f66d3bb
Felipe Camara <fcamara@redventures.com>
2022-10-01 13:28:18 -0300

    feat: adding last news section to home page

---
+++ frontend/src/pages/Home/components/LastNews/index.jsx
@@ -0,0 +1,16 @@
+import ImageCardsList from "../ImageCardsList";
+
+import style from './LastNews.module.scss';
+
+export default function LastNews({title}) {
+  return (
+    <section className={style.container}>
+      <h1 className={style.title}>{title}</h1>
+      <ImageCardsList />
+      <img 
+        src={'/images/square-dotted.svg'}
+        className={style.image}  
+      />
+    </section>
+  )
+}
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import Hero from './components/Hero';
+
 import styles from './style.module.scss';
 
 export default function Home() {
