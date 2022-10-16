e7bc2b13ca01cd20a6e04edb22fc669b8f7289f2
Walderley Neto <89034168+NetoOSantos@users.noreply.github.com>
2022-10-16 10:58:52 -0300

    selection sort (#26)
    Co-authored-by: Walderley De Oliveira Santos Neto <woneto@stefanini.com>

--- backend/elixir/src/main/java/com/example/elixir/controle/InsigniaController.java
+++ backend/elixir/src/main/java/com/example/elixir/controle/InsigniaController.java
@@ -1,10 +1,14 @@
 package com.example.elixir.controle;
 
+import com.example.elixir.dominio.Insignia;
+import com.example.elixir.dominio.InsigniaDoador;
 import com.example.elixir.repositorio.DoadorRepository;
 import com.example.elixir.repositorio.InsigniaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/insignia")
@@ -13,6 +17,19 @@ public class InsigniaController {
     @Autowired
     private InsigniaRepository repository;
 
+    @PostMapping
+    public ResponseEntity<Insignia> post(@RequestBody @Valid Insignia novaInsigia) {
+        repository.save(novaInsigia);
+        return ResponseEntity.status(201).body(novaInsigia);
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable @Valid int id){
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return ResponseEntity.status(200).build();
+        }
+        return ResponseEntity.status(404).build();
+    }
 
 }
--- backend/elixir/src/main/java/com/example/elixir/repositorio/PublicacaoRepository.java
+++ backend/elixir/src/main/java/com/example/elixir/repositorio/PublicacaoRepository.java
@@ -1,8 +1,13 @@
 package com.example.elixir.repositorio;
 
 import com.example.elixir.dominio.Publicacao;
+import com.example.elixir.dominio.Receptor;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface PublicacaoRepository extends JpaRepository<Publicacao, Integer> {
 
+    List<Receptor> findByDtPublicacao(LocalDate dtPublicacao);
 }
